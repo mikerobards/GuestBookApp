ba7c3956d04c387b7b6f416e0e0abce9955d0296
Michael Robards <mikerobards@icloud.com>
2022-01-25 17:31:06 -0500

    Met all requirements

---
+++ GuestBook/GuestBookMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook
+{
+    internal class GuestBookMethods
+    {
+        public static void WelcomeUser()
+        {
+            Console.WriteLine("Welcome to the Guest Book App!");
+            Console.WriteLine("TM Mike Robards 2022");
+            Console.WriteLine();
+        }
+        public static string AskUsersName()
+        {
+            Console.Write("What is the name of your party: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+
+    }
+}
--- GuestBook/Program.cs
+++ GuestBook/Program.cs
@@ -1,4 +1,56 @@
-﻿
+﻿using GuestBook;
+
+List<string> guestNames = new List<string>();
+int guestCount = 0;
+string anotherParty;
+bool isValidNum;
+
+GuestBookMethods.WelcomeUser();
+
+//GuestBookMethods.AskUsersName();
+
+//GuestBookMethods.AskNumberGuests(guestCount);
+
+
+do
+{
+
+    string userName = GuestBookMethods.AskUsersName();
+    guestNames.Add(userName);
+
+    do
+    {
+        Console.Write("How many are in your party: ");
+        string numValue = Console.ReadLine();
+        isValidNum = int.TryParse(numValue, out int num);
+        if (isValidNum)
+        {
+            guestCount += num;
+        }
+        else
+        {
+            Console.WriteLine("That is not a valid number! Try again.");
+        } 
+    } while (!isValidNum);
+    
+    Console.Write("Enter another party? (y/n): ");
+    anotherParty = Console.ReadLine();
+
+} while (anotherParty == "y");
+
+Console.WriteLine();
+
+foreach (var name in guestNames)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine();
+
+Console.WriteLine(guestCount);
+
+
+
 
 
 
